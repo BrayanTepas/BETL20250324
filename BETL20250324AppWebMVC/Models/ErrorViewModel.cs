77c5f4dc73d9207e12c0befd07c19d6ce133aca4
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-03-24 07:45:59 -0600

    Agregar archivos de proyecto.

---
+++ BETL20250324AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BETL20250324AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
